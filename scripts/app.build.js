212752aea57db41fe67c84f961540001cb13a655
jaykobpc <watenga.jacob@gmail.com>
2020-11-22 13:01:03 +0300

    updates

--- scripts/app.build.js
+++ scripts/app.build.js
@@ -20,46 +20,44 @@ if (wx_mainview) {
   });
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-  if(document.body.classList.contains("preload")) {
+document.addEventListener("DOMContentLoaded", function () {
+  if (document.body.classList.contains("preload")) {
     document.body.classList.remove("preload");
   }
-})
-
+});
 
 allImages.forEach(function (img) {
   img.setAttribute("draggable", false);
-  img.addEventListener("contextmenu", function(e) {
+  img.addEventListener("contextmenu", function (e) {
     e.preventDefault();
-  })
+  });
 });
 
-if(mobileNavItem) {
-  mobileNavItem.forEach(function(btn) {
-    btn.addEventListener("click", function(e) {
-      e.stopPropagation()
-    })
-  })
+if (mobileNavItem) {
+  mobileNavItem.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+      e.stopPropagation();
+    });
+  });
 }
 
-if(mobileNavBtn) {
-  mobileNavBtn.addEventListener("click", function(e) {
+if (mobileNavBtn) {
+  mobileNavBtn.addEventListener("click", function (e) {
     e.preventDefault();
     mobileNavContainer.classList.add("mobilenav_open");
   });
 }
 
-if(mobileNavContainer) {
-  mobileNavContainer.addEventListener("click", function(e) {
+if (mobileNavContainer) {
+  mobileNavContainer.addEventListener("click", function (e) {
     e.preventDefault();
     e.stopPropagation();
     e.stopImmediatePropagation();
     mobileNavContainer.classList.remove("mobilenav_open");
-  })
+  });
 }
 
-
-;(function () {
+(function () {
   "use strict";
 
   function KtWidget(params) {
@@ -82,7 +80,7 @@ if(mobileNavContainer) {
       self.selectorWidget = params.selectorWidget;
     }
 
-    if(params.classSelector) {
+    if (params.classSelector) {
       self.classSelector = params.classSelector;
     }
 
@@ -114,7 +112,7 @@ if(mobileNavContainer) {
         event.target != selectorAsWidget &&
         event.target.parentNode != selectorAsWidget
       ) {
-        if(selectorAsWidget) {
+        if (selectorAsWidget) {
           selectorAsWidget.classList.remove(self.classSelector);
         }
       }
@@ -124,25 +122,137 @@ if(mobileNavContainer) {
   window.KtWidget = KtWidget;
 })();
 
+new KtWidget({
+  selectorOpen: "#card-listopen-1",
+  selectorWidget: "#card-listview-1",
+  classSelector: "show_listview",
+});
+new KtWidget({
+  selectorOpen: "#card-listopen-2",
+  selectorWidget: "#card-listview-2",
+  classSelector: "show_listview",
+});
+new KtWidget({
+  selectorOpen: "#card-listopen-3",
+  selectorWidget: "#card-listview-3",
+  classSelector: "show_listview",
+});
+new KtWidget({
+  selectorOpen: "#card-listopen-4",
+  selectorWidget: "#card-listview-4",
+  classSelector: "show_listview",
+});
+new KtWidget({
+  selectorOpen: "#card-listopen-5",
+  selectorWidget: "#card-listview-5",
+  classSelector: "show_listview",
+});
+new KtWidget({
+  selectorOpen: "#card-listopen-6",
+  selectorWidget: "#card-listview-6",
+  classSelector: "show_listview",
+});
+new KtWidget({
+  selectorOpen: "#card-listopen-7",
+  selectorWidget: "#card-listview-7",
+  classSelector: "show_listview",
+});
+new KtWidget({
+  selectorOpen: "#card-listopen-8",
+  selectorWidget: "#card-listview-8",
+  classSelector: "show_listview",
+});
+new KtWidget({
+  selectorOpen: "#card-listopen-9",
+  selectorWidget: "#card-listview-9",
+  classSelector: "show_listview",
+});
+new KtWidget({
+  selectorOpen: "#card-listopen-10",
+  selectorWidget: "#card-listview-10",
+  classSelector: "show_listview",
+});
+new KtWidget({
+  selectorOpen: "#card-listopen-11",
+  selectorWidget: "#card-listview-11",
+  classSelector: "show_listview",
+});
+new KtWidget({
+  selectorOpen: "#card-listopen-12",
+  selectorWidget: "#card-listview-12",
+  classSelector: "show_listview",
+});
+new KtWidget({
+  selectorOpen: "#card-listopen-13",
+  selectorWidget: "#card-listview-13",
+  classSelector: "show_listview",
+});
+new KtWidget({
+  selectorOpen: "#card-listopen-14",
+  selectorWidget: "#card-listview-14",
+  classSelector: "show_listview",
+});
+new KtWidget({
+  selectorOpen: "#card-listopen-15",
+  selectorWidget: "#card-listview-15",
+  classSelector: "show_listview",
+});
+new KtWidget({
+  selectorOpen: "#card-listopen-16",
+  selectorWidget: "#card-listview-16",
+  classSelector: "show_listview",
+});
+new KtWidget({
+  selectorOpen: "#navbar_open",
+  selectorWidget: "#navbar_drop",
+  classSelector: "show_dropdown",
+});
+new KtWidget({
+  selectorOpen: "#navbar_open",
+  selectorWidget: "#navbar_drop",
+  classSelector: "show_dropdown",
+});
+new KtWidget({
+  selectorOpen: "#sidenav_explore",
+  selectorWidget: "#sidenav_explore_open",
+  classSelector: "show_side",
+});
+new KtWidget({
+  selectorOpen: "#exit-btn",
+  selectorWidget: "#exit-modal",
+  classSelector: "show_exit_modal",
+});
 
-new KtWidget({ selectorOpen: "#card-listopen-1", selectorWidget: "#card-listview-1", classSelector: "show_listview" });
-new KtWidget({ selectorOpen: "#card-listopen-2", selectorWidget: "#card-listview-2", classSelector: "show_listview" });
-new KtWidget({ selectorOpen: "#card-listopen-3", selectorWidget: "#card-listview-3", classSelector: "show_listview" });
-new KtWidget({ selectorOpen: "#card-listopen-4", selectorWidget: "#card-listview-4", classSelector: "show_listview" });
-new KtWidget({ selectorOpen: "#card-listopen-5", selectorWidget: "#card-listview-5", classSelector: "show_listview" });
-new KtWidget({ selectorOpen: "#card-listopen-6", selectorWidget: "#card-listview-6", classSelector: "show_listview" });
-new KtWidget({ selectorOpen: "#card-listopen-7", selectorWidget: "#card-listview-7", classSelector: "show_listview" });
-new KtWidget({ selectorOpen: "#card-listopen-8", selectorWidget: "#card-listview-8", classSelector: "show_listview" });
-new KtWidget({ selectorOpen: "#card-listopen-9", selectorWidget: "#card-listview-9", classSelector: "show_listview" });
-new KtWidget({ selectorOpen: "#card-listopen-10", selectorWidget: "#card-listview-10", classSelector: "show_listview" });
-new KtWidget({ selectorOpen: "#card-listopen-11", selectorWidget: "#card-listview-11", classSelector: "show_listview" });
-new KtWidget({ selectorOpen: "#card-listopen-12", selectorWidget: "#card-listview-12", classSelector: "show_listview" });
-new KtWidget({ selectorOpen: "#card-listopen-13", selectorWidget: "#card-listview-13", classSelector: "show_listview" });
-new KtWidget({ selectorOpen: "#card-listopen-14", selectorWidget: "#card-listview-14", classSelector: "show_listview" });
-new KtWidget({ selectorOpen: "#card-listopen-15", selectorWidget: "#card-listview-15", classSelector: "show_listview" });
-new KtWidget({ selectorOpen: "#card-listopen-16", selectorWidget: "#card-listview-16", classSelector: "show_listview" });
-new KtWidget({ selectorOpen: "#navbar_open", selectorWidget: "#navbar_drop", classSelector: "show_dropdown" });
-new KtWidget({ selectorOpen: "#navbar_open", selectorWidget: "#navbar_drop", classSelector: "show_dropdown" });
-new KtWidget({ selectorOpen: "#sidenav_explore", selectorWidget: "#sidenav_explore_open", classSelector: "show_side" });
-new KtWidget({ selectorOpen: "#exit-btn", selectorWidget: "#exit-modal", classSelector: "show_exit_modal" });
+(function () {
+  "use strict";
 
+  window.addEventListener("load", function () {
+    var counter = 0;
+    var progressloader = document.querySelector(".progressbar__innerbar");
+    var appLoaderWindow = document.querySelector("#apploader");
+
+    /**show loading screen */
+    //appLoaderWindow.classList.add("loader-fade");
+
+    /** Interval */
+    var Loader = setInterval(isLoading, 80);
+
+    /** Hide scroll bar when modal is open */
+    document.body.style.overflowY = "hidden";
+    /** hide loading screen */
+
+    function isLoading() {
+      if (counter >= 100) {
+        clearInterval(Loader);
+        /** show scroll bar when loading is complete */
+        document.body.style.overflowY = "auto";
+        /** set loading state as false */
+       appLoaderWindow.classList.add("loader-fade");
+
+      } else {
+        counter++;
+        progressloader.style.width = counter + "%";
+      }
+    }
+  });
+})();
